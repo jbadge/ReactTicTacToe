c3d5b1786fd18c214f435f26ac84521083d335d6
Jeremy Badger <jeremebadger@gmail.com>
2024-02-13 15:55:46 -0800

    Manual state, validated working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  const [game, setGame] = useState({
+    board: [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+    ],
+    id: null,
+    winner: null,
+  })
+
+  return (
+    <div>
+      <h1>
+        Tic Tac Toe - <button>New</button>
+      </h1>
+      <ul>
+        <li>{game.board[0][0]}</li>
+        <li>{game.board[0][1]}</li>
+        <li>{game.board[0][2]}</li>
+        <li>{game.board[1][0]}</li>
+        <li>{game.board[1][1]}</li>
+        <li>{game.board[1][2]}</li>
+        <li>{game.board[2][0]}</li>
+        <li>{game.board[2][1]}</li>
+        <li>{game.board[2][2]}</li>
+      </ul>
+    </div>
+  )
 }
