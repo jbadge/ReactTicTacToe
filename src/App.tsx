90edb73b670a2024d49899d308b1f0c1b9f26b23
Jeremy Badger <jeremebadger@gmail.com>
2024-02-14 12:25:32 -0800

    Added API to create new game

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,86 @@ export function App() {
     winner: null,
   })
 
+  function handleClickCell(row: number, column: number) {
+    console.log({ row, column })
+  }
+
+  async function handleNewGame() {
+    // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    if (response.ok) {
+      const newGameState = await response.json()
+      setGame(newGameState)
+    }
+  }
+
   return (
     <div>
       <h1>
-        Tic Tac Toe - <button>New</button>
+        Tic Tac Toe - {game.id} <button onClick={handleNewGame}>New</button>
       </h1>
       <ul>
-        <li>{game.board[0][0]}</li>
-        <li>{game.board[0][1]}</li>
-        <li>{game.board[0][2]}</li>
-        <li>{game.board[1][0]}</li>
-        <li>{game.board[1][1]}</li>
-        <li>{game.board[1][2]}</li>
-        <li>{game.board[2][0]}</li>
-        <li>{game.board[2][1]}</li>
-        <li>{game.board[2][2]}</li>
+        <li
+          onKeyDown={() => handleClickCell(0, 0)}
+          onClick={() => handleClickCell(0, 0)}
+        >
+          {game.board[0][0]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(0, 1)}
+          onClick={() => handleClickCell(0, 1)}
+        >
+          {game.board[0][1]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(0, 2)}
+          onClick={() => handleClickCell(0, 2)}
+        >
+          {game.board[0][2]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(1, 0)}
+          onClick={() => handleClickCell(1, 0)}
+        >
+          {game.board[1][0]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(1, 1)}
+          onClick={() => handleClickCell(1, 1)}
+        >
+          {game.board[1][1]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(1, 2)}
+          onClick={() => handleClickCell(1, 2)}
+        >
+          {game.board[1][2]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(2, 0)}
+          onClick={() => handleClickCell(2, 0)}
+        >
+          {game.board[2][0]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(2, 1)}
+          onClick={() => handleClickCell(2, 1)}
+        >
+          {game.board[2][1]}
+        </li>
+        <li
+          onKeyDown={() => handleClickCell(2, 2)}
+          onClick={() => handleClickCell(2, 2)}
+        >
+          {game.board[2][2]}
+        </li>
       </ul>
     </div>
   )
